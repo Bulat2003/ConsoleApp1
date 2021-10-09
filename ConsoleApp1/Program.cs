1ae5e3f4cb504da2f132ec8660ee30fb052109d6
ZZMingaliev <ZZMingaliev@KIT-07-204-00>
2021-10-09 09:46:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, l;
+
+            k = Convert.ToInt32(Console.ReadLine());
+            l = Convert.ToInt32(Console.ReadLine());
+
+            int tempSum;
+            int sum = 0;
+
+            for (k = 1; k <= 10; k++)
+            {
+                tempSum = Convert.ToInt32(Math.Pow(k, 3));
+
+                for (l = 1; l <= 15; l++)
+                {
+                    sum += tempSum * (k - l) * (k - l);
+                }
+            }
+
+            Console.WriteLine("Summa = " + sum);
+            Console.ReadKey();
+        }
+    }
+}
